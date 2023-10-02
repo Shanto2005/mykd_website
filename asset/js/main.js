05d8b0002e14d87c3ec26fd66d983b0b3f634a02
Shanto2005 <ri4439392@robiulu@example.com>
2023-10-03 01:30:40 +0600

    card

--- asset/js/main.js
+++ asset/js/main.js
@@ -7,14 +7,14 @@
 //     }
 // })
 
-var cursor = document.getElementById("cursor");
-  document.onmousemove = function(e){
-    cursor.style.left = (e.pageX - 25) + "px";
-    cursor.style.top = (e.pageY - 25) + "px";
-    cursor.style.display = "block";
-    cursor.style.display = "block";
+// var cursor = document.getElementById("cursor");
+//   document.onmousemove = function(e){
+//     cursor.style.left = (e.pageX - 25) + "px";
+//     cursor.style.top = (e.pageY - 25) + "px";
+//     cursor.style.display = "block";
+//     cursor.style.display = "block";
 
-  }
+//   }
 
 document.addEventListener(
     "DOMContentLoaded", () => {
